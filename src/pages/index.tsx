5229b4e7929dca2fd7bf5920c459360a6d649139
lucas barra <lucastbarra@hotmail.com>
2021-06-29 12:01:34 -0300

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
-import { StringifyOptions } from 'querystring';
 
 interface HomeProps {
   product: {
@@ -38,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1J7KF5Ked2EGM3bY2mjCKhSj');
 
@@ -53,6 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
-  }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
 }
