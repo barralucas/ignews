5355333e7bc6c47f968588f2177f6dcd0c2225eb
lucas barra <lucastbarra@hotmail.com>
2021-07-08 17:00:38 -0300

    checkout's session stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribebuttonProps {
@@ -5,10 +6,25 @@ interface SubscribebuttonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribebuttonProps) {
+
+    const [session] = useSession();
+
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+
+        
+    }
+
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
