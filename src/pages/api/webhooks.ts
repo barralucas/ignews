694ddd9336e6ccf76a0044dcd72d8f27f6ee5295
Lucas Barra <lucastbarra@hotmail.com>
2021-07-19 12:24:17 -0300

    config prismic cms

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,6 +24,7 @@ export const config = {
 
 const relevantEvents = new Set([
     'checkout.session.completed',
+    'customer.subscription.created',
     'customer.subscription.updated',
     'customer.subscription.deleted'
 ]);
@@ -47,6 +48,7 @@ export default async function webhooks(req: NextApiRequest, res: NextApiResponse
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
+                    case 'customer.subscription.created':
                     case 'customer.subscription.updated':
                     case 'customer.subscription.deleted':
                         const subscription = event.data.object as Stripe.Subscription;
@@ -56,7 +58,7 @@ export default async function webhooks(req: NextApiRequest, res: NextApiResponse
                         await saveSubscription(
                             subscription.id,
                             subscription.customer.toString(),
-                            false
+                            type === 'customer.subscription.created'
                         );
 
                         break;
