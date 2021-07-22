0051a6dc14327f32107b2f3563a2ee73a36569cc
Lucas Barra <lucastbarra@hotmail.com>
2021-07-22 10:52:25 -0300

    API prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,35 +1,56 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
 import styles from './styles.module.scss';
 
 export default function Posts() {
     return (
         <>
-        <Head>
-            <title>Posts | ignews</title>
-        </Head>
+            <Head>
+                <title>Posts | ignews</title>
+            </Head>
 
-        <main className={styles.container}>
-            <div className={styles.posts}>
-                <a href="#">
-                    <time>12 de março de 2021</time>
-                    <strong>Lorem Ipsum</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
-                </a>
-                <a href="#">
-                    <time>12 de março de 2021</time>
-                    <strong>Lorem Ipsum</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
-                </a><a href="#">
-                    <time>12 de março de 2021</time>
-                    <strong>Lorem Ipsum</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
-                </a><a href="#">
-                    <time>12 de março de 2021</time>
-                    <strong>Lorem Ipsum</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
-                </a>
-            </div>
-        </main>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Lorem Ipsum</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Lorem Ipsum</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
+                    </a><a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Lorem Ipsum</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
+                    </a><a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Lorem Ipsum</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse voluptatibus sunt eaque culpa repellat praesentium officiis aspernatur quidem reprehenderit veritatis ducimus commodi accusantium neque distinctio, voluptatum, magnam ad fuga.</p>
+                    </a>
+                </div>
+            </main>
         </>
     );
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publication')
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100
+    });
+
+    console.log(JSON.stringify(response, null, 2));
+
+    return {
+        props: {}
+    }
 }
