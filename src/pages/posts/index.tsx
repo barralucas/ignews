20c24ad1a19fea6d2d80d07a902f105313478871
Lucas Barra <lucastbarra@hotmail.com>
2021-07-23 10:07:30 -0300

    page post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,12 +4,14 @@ import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
 import styles from './styles.module.scss';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
+
 
 type Post = {
     slug: string;
     title: string;
     excerpt: string;
-    updatedAt: string;
+    updateAt: string;
 }
 
 interface PostsProps {
@@ -26,11 +28,13 @@ export default function Posts({ posts }: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                        <a href="#" key={post.slug}>
-                            <time>{post.updatedAt}</time>
+                        <Link href={`/posts/${post.slug}`} key={post.slug}>
+                        <a>
+                            <time>{post.updateAt}</time>
                             <strong>{post.title}</strong>
                             <p>{post.excerpt}</p>
                         </a>
+                        </Link>
                     ))}
                 </div>
             </main>
@@ -51,7 +55,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
     const posts = response.results.map(post => {
         return {
-            skug: post.uid,
+            slug: post.uid,
             title: RichText.asText(post.data.title),
             excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
             updateAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
